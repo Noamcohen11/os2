783f2b4668a027c8a2e87356728d88a533140de1
Noamcohen11 <noam.cohen11@mail.huji.ac.il>
2024-06-14 15:32:01 +0300

    auto commit

--- uthreads.cpp
+++ uthreads.cpp
@@ -388,16 +388,16 @@ int uthread_spawn(thread_entry_point entry_point)
         unblock_sig(SIGVTALRM);
         return -1;
     }
-    char *stack = NULL;
-    try
-    {
-        char *stack = new char[STACK_SIZE];
-    }
-    catch (std::bad_alloc &e)
-    {
-        std::cerr << SYS_ERROR << "malloc failure.\n";
-        return -1;
-    }
+    // char *stack = nullptr;
+    // try
+    // {
+    char *stack = new char[STACK_SIZE];
+    // }
+    // catch (std::bad_alloc &e)
+    // {
+    // std::cerr << SYS_ERROR << "malloc failure.\n";
+    //     return -1;
+    // }
     int tid = __find_available_tid();
     readyQueue->push_back(tid);
     __setup_thread(tid, stack, entry_point);
